1ef47a92e163d83b7ae0153508eecc049459f1f4
SeungwooSeo <prodvssw.98@gmail.com>
2023-12-26 10:18:16 +0900

    Initial Commit

---
+++ SeSACWebSocket/SeSACWebSocketApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeSACWebSocketApp.swift
+//  SeSACWebSocket
+//
+//  Created by 서승우 on 2023/12/26.
+//
+
+import SwiftUI
+
+@main
+struct SeSACWebSocketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
